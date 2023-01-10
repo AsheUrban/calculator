5a6be69f92d884c3fde8bc4b62ce75a8da65eb1b
Ashe Urban <ashe@goldentongue.com>
2023-01-09 16:56:18 -0800

    FIX sub variable in UI.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -14,18 +15,13 @@ function div(number1, number2) {
   return number1 / number2;
 }
 
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another Number:"));
-// const result = div(number1, number2);
-
-// alert(result);
+//user interface
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another Number:"));
-const result = mult(number1, number2);
+// const result = mult(number1, number2);
 
-alert(result);
-
+// alert(result);
 
 // Alternatively...
 // window.alert("The difference of your numbers =" + subtract(number1, number2) + ".");
@@ -34,46 +30,46 @@ alert(result);
 // window.alert("The product of your numbers =" + mult(number1, number2) + ".");
 
 //Or...
-// window.alert(
-//   "Your numbers were: " +
-//     number1 +
-//     " and " +
-//     number2 +
-//     ". " +
-//     "The difference of your numbers =" +
-//     subtract(number1, number2) +
-//     "."
-// );
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The difference of your numbers =" +
+    sub(number1, number2) +
+    "."
+);
 
-// window.alert(
-//   "Your numbers were: " +
-//     number1 +
-//     " and " +
-//     number2 +
-//     ". " +
-//     "The sum of your numbers =" +
-//     add(number1, number2) +
-//     "."
-// );
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The sum of your numbers =" +
+    add(number1, number2) +
+    "."
+);
 
-// window.alert(
-//   "Your numbers were: " +
-//     number1 +
-//     " and " +
-//     number2 +
-//     ". " +
-//     "The remainder of your numbers =" +
-//     div(number1, number2) +
-//     "."
-// );
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The remainder of your numbers =" +
+    div(number1, number2) +
+    "."
+);
 
-// window.alert(
-//   "Your numbers were: " +
-//     number1 +
-//     " and " +
-//     number2 +
-//     ". " +
-//     "The product of your numbers =" +
-//     mult(number1, number2) +
-//     "."
-// );
+window.alert(
+  "Your numbers were: " +
+    number1 +
+    " and " +
+    number2 +
+    ". " +
+    "The product of your numbers =" +
+    mult(number1, number2) +
+    "."
+);
